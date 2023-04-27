9cab7f3581a41d30a7e5913de6168a3c053ca068
Shawn Patel <shawnpatel16@gmail.com>
2023-04-27 15:59:53 -0700

    styled workout form, added autosuggestions

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
+import "./modal-transition.css";
 
 const Modal = ({ isOpen, onClose, title, children }) => {
-  if (!isOpen) return null;
-
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
-      <div className="relative w-full max-w-md p-6 bg-white rounded shadow-lg">
-        <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-          onClick={onClose}
-        >
-          &times;
-        </button>
-        {title && <h2 className="mb-4 text-xl font-semibold">{title}</h2>}
-        <div className="modal-body">{children}</div>
+    <CSSTransition in={isOpen} timeout={300} classNames="modal" unmountOnExit>
+      <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
+        <div className="relative w-full sm:max-w-md md:max-w-lg lg:max-w-xl max-w-sm p-8  bg-primary rounded shadow-lg max-h-screen overflow-y-auto">
+          <button
+            className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+            onClick={onClose}
+          >
+            &times;
+          </button>
+          {title && <h2 className="mb-4 text-xl font-semibold text-white">{title}</h2>}
+          <div className="modal-body">{children}</div>
+        </div>
       </div>
-    </div>
+    </CSSTransition>
   );
 };
 
--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import { Formik, Form, useField, useFormikContext, Field } from "formik";
-import {AiOutlinePlus}  from "react-icons/ai";
+import React, {useState} from 'react'
+import { Formik, Form, useField, useFormikContext, Field, FieldArray } from "formik";
+import { FiTrash2 } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
+import Autosuggest from "react-autosuggest";
+import "./autosuggest.css";
+import "./workoutForm.css"
 import * as Yup from "yup";
 const suggestedExercises = {
   "Upper Body": [
@@ -39,7 +43,7 @@ const suggestedExercises = {
     "Good Morning",
     "Leg Extensions",
   ],
-  Push: [
+  "Push": [
     "Bench Press",
     "Overhead Press",
     "Dumbbell Bench Press",
@@ -49,7 +53,7 @@ const suggestedExercises = {
     "Dumbbell Fly",
     "Cable Tricep Extension",
   ],
-  Pull: [
+  "Pull": [
     "Lat Pulldown",
     "Cable Row",
     "Dumbbell Row",
@@ -63,7 +67,7 @@ const suggestedExercises = {
     "Pull-ups",
     "Chin-ups",
   ],
-  Legs: [
+  "Legs": [
     "Squat",
     "Deadlift",
     "Lunge",
@@ -114,7 +118,29 @@ const suggestedExercises = {
   "Legs/Shoulders": ["Squat","Overhead Press","Lunge","Leg Press","Leg Extension","Leg Curl","Calf Raise","Romanian Deadlift","Good Morning","Leg Extensions","Lateral Raise","Dumbbell Overhead Press","Dumbbell Rear Delt Fly","Dumbbell Shrug"],
 };
 const WorkoutForm = ({closeModal}) => {
+  const [suggestions, setSuggestions] = useState([]);
+  const getSuggestions = (value,splitName) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    console.log(value)
+    const splitExercises = suggestedExercises[splitName] || [];
+    console.log(splitExercises)
+    return inputLength === 0
+      ? []
+      : splitExercises.filter(
+          (exercise) =>
+            exercise.toLowerCase().slice(0, inputLength) === inputValue
+        );
+  };
+  const onSuggestionsFetchRequested = ({ value }, splitName) => {
+    setSuggestions(getSuggestions(value, splitName));
+  };
 
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([]);
+  };
+
+  const getSuggestionValue = (suggestion) => suggestion;
   
   const handleSubmit = (values) => {
     console.log(values);
@@ -130,80 +156,159 @@ const WorkoutForm = ({closeModal}) => {
       onSubmit={handleSubmit}
     >
       {({ values, handleChange, setFieldValue }) => {
-        const addExercise = () => {
-          setFieldValue('exercises', [
-            ...values.exercises,
-            { name: '', sets: [] },
-          ]);
-        };
+        
         return (
-          <Form>
-            <label htmlFor="splitName">Split Name</label>
-            <Field as="select" name="splitName">
-              <option value="">Select a split</option>
-              <option value="Upper Body">Upper Body</option>
-              <option value="Lower Body">Lower Body</option>
-              <option value="Push">Push</option>
-              <option value="Push">Pull</option>
-              <option value="Legs">Legs</option>
-              <option value="Full Body">Full Body</option>
-              <option value="Chest/Back">Chest/Back</option>
-              <option value="Arms">Arms</option>
-              <option value="Legs/Shoulders">Legs/Shoulders</option>
-            </Field>
+          <Form className="space-y-6 workout-form">
+            <div className="flex flex-wrap items-center space-x-4 split-name-wrapper">
+              <label htmlFor="splitName" className="text-lg split-name-label">
+                Split Name
+              </label>
+              <Field
+                as="select"
+                name="splitName"
+                className="rounded split-name-select"
+              >
+                <option value="">Select a split</option>
+                <option value="Upper Body">Upper Body</option>
+                <option value="Lower Body">Lower Body</option>
+                <option value="Push">Push</option>
+                <option value="Push">Pull</option>
+                <option value="Legs">Legs</option>
+                <option value="Full Body">Full Body</option>
+                <option value="Chest/Back">Chest/Back</option>
+                <option value="Arms">Arms</option>
+                <option value="Legs/Shoulders">Legs/Shoulders</option>
+              </Field>
+            </div>
+            
+            <FieldArray name="exercises">
+              {({ remove, push }) => (
+                <>
+                  {values.exercises.map((exercise, index) => (
+                    <div key={index} className="space-y-4  exercise-wrapper">
+                      <div className="flex items-center space-x-4  exercise-row">
+                        <label
+                          className="exercise-name-label"
+                          htmlFor={`exercises.${index}.name`}
+                        >
+                          Exercise {index + 1}
+                        </label>
+                        <Autosuggest
+                          suggestions={suggestions}
+                          theme={{container:'autosuggest-container'}}
+                          onSuggestionsFetchRequested={({ value }) =>
+                            onSuggestionsFetchRequested(
+                              { value },
+                              values.splitName
+                            )
+                          }
+                          onSuggestionsClearRequested={
+                            onSuggestionsClearRequested
+                          }
+                          getSuggestionValue={getSuggestionValue}
+                          renderSuggestion={(suggestion) => (
+                            <div className="suggestion-item">{suggestion}</div>
+                          )}
+                          inputProps={{
+                            type: "text",
+                            name: `exercises.${index}.name`,
+                            value: exercise.name,
+                            onChange: (event, { newValue }) => {
+                              setFieldValue(
+                                `exercises.${index}.name`,
+                                newValue
+                              );
+                            },
+                            className: "h-6 exercise-input",
+                          }}
+                        />
 
-            <label htmlFor="exercises">
-              Exercise
-              <button onClick={addExercise}>
-                <AiOutlinePlus size={12} />
-              </button>
-            </label>
-            {values.exercises.map((exercise, index) => (
-              <div key={index}>
-                <label htmlFor={`exercises.${index}.name`}>
-                  Exercise {index + 1}
-                </label>
-                <input
-                  type="text"
-                  name={`exercises.${index}.name`}
-                  value={exercise.name}
-                  onChange={handleChange}
-                />
-                <button
-                  type="button"
-                  onClick={() => {
-                    const newSets = [...exercise.sets, { weight: "", reps: "" }];
-                    setFieldValue(`exercises.${index}.sets`, newSets);
-                  }}
-                >
-                  <AiOutlinePlus size={10} />
-                </button>
-                {exercise.sets.map((set, setIndex) => (
-                  <div key={setIndex}>
-                    <label htmlFor={`exercises.${index}.sets.${setIndex}.weight`}>
-                      Weight
-                    </label>
-                    <input
-                      type="number"
-                      name={`exercises.${index}.sets.${setIndex}.weight`}
-                      value={set.weight}
-                      onChange={handleChange}
-                    />
-                    <label htmlFor={`exercises.${index}.sets.${setIndex}.reps`}>
-                      Reps
-                    </label>
-                    <input
-                      type="number"
-                      name={`exercises.${index}.sets.${setIndex}.reps`}
-                      value={set.reps}
-                      onChange={handleChange}
-                    />
+                        <button
+                          type="button"
+                          onClick={() => remove(index)}
+                          className="text-red-600  delete-exercise-btn"
+                        >
+                          <FiTrash2 />
+                        </button>
+                      </div>
+                      <div className="ml-4">
+                        <FieldArray name={`exercises.${index}.sets`}>
+                          {({ remove: removeSet, push: pushSet }) => (
+                            <>
+                              {exercise.sets.map((set, setIndex) => (
+                                <div
+                                  key={setIndex}
+                                  className="flex items-center space-x-4 set-wrapper"
+                                >
+                                  <span className="set-label">
+                                    Set {setIndex + 1}
+                                  </span>
+                                  <label
+                                    htmlFor={`exercises.${index}.sets.${setIndex}.weight`}
+                                    className="weight-label"
+                                  >
+                                    Weight
+                                  </label>
+                                  <input
+                                    type="number"
+                                    name={`exercises.${index}.sets.${setIndex}.weight`}
+                                    value={set.weight}
+                                    onChange={handleChange}
+                                    className="w-20 weight-input"
+                                  />
+                                  <label
+                                    htmlFor={`exercises.${index}.sets.${setIndex}.reps`}
+                                    className="reps-label"
+                                  >
+                                    Reps
+                                  </label>
+                                  <input
+                                    type="number"
+                                    name={`exercises.${index}.sets.${setIndex}.reps`}
+                                    value={set.reps}
+                                    onChange={handleChange}
+                                    className="w-20 reps-input"
+                                  />
+                                  <button
+                                    type="button"
+                                    onClick={() => removeSet(setIndex)}
+                                    className="text-red-600 delete-set-btn"
+                                  >
+                                    <FiTrash2 />
+                                  </button>
+                                </div>
+                              ))}
+                              <button
+                                type="button"
+                                onClick={() =>
+                                  pushSet({ weight: "", reps: "" })
+                                }
+                                className='add-set-btn'
+                              >
+                                <AiOutlinePlus size={15} />
+                              </button>
+                            </>
+                          )}
+                        </FieldArray>
+                      </div>
+                    </div>
+                  ))}
+
+                  <div className="flex items-center add-exercise-wrapper">
+                    <button
+                      type="button"
+                      onClick={() => push({ name: "", sets: [] })}
+                      className="add-exercise-btn"
+                    >
+                      <AiOutlinePlus />
+                    </button>
                   </div>
-                ))}
-              </div>
-            ))}
+                </>
+              )}
+            </FieldArray>
+            <button type="submit">Submit</button>
           </Form>
-        )
+        );
       }}
     </Formik>
   );
