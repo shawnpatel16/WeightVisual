873902edcfb347a3166f1ea7e01c2bc71e8eab2d
Shawn Patel <shawnpatel16@gmail.com>
2023-05-26 13:57:56 -0700

    Containerizing frontend,backend,database into docker containers.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: "0.0.0.0",
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://backend:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
