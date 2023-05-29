877061c8c47ee01cda6b5cb7d214e2dcb72b586f
Shawn Patel <shawnpatel16@gmail.com>
2023-05-29 13:29:49 -0700

    local build

--- backend/prisma/migrations/20230505183954_revert/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `test` on the `ExerciseSets` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "ExerciseSets" DROP COLUMN "test";
